be2c15aea6898791649e2eea8897b9863b854c58
IgroPoloniaeCinco <rogalik709@gmail.com>
2024-05-27 12:24:19 +0300

    navbar edit

--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
 <nav>
     <ul id="navbar">
-        <a href="index.html" class="m-link"><li id="no-border" class="m-item">HOME // Αρχική</li></a>
+        <a href="index.html" class="m-link"><li id="no-border" class="m-item">HOME // ΑΡΧΙΚΉ</li></a>
         <a href="about-us.php" class="m-link"><li class="m-item">ABOUT US // ΣΧΕΤΙΚΑ ΜΕ ΕΜΑΣ</li></a>
-        <a href="courses-work.php" class="m-link"><li class="m-item">COURSES-WORKSHOPS</li></a>
+        <a href="courses-work.php" class="m-link"><li class="m-item">COURSES-WORKSHOPS // ΜΑΘΗΜΑΤΑ-ΕΡΓΑΣΤΗΡΙΑ</li></a>
         <a href="erasmus.php" class="m-link"><li class="m-item">ERASMUS</li></a>
-        <a href="events.php" class="m-link"><li class="m-item">EVENTS</li></a>
-        <a href="archive.php" class="m-link"><li class="m-item">ARCHIVE</li></a>
-        <a href="contact-us" class="m-link"><li class="m-item">CONTACT US</li></a>
+        <a href="events.php" class="m-link"><li class="m-item">EVENTS // ΕΚΔΗΛΩΣΕΙΣ</li></a>
+        <a href="archive.php" class="m-link"><li class="m-item">ARCHIVE // ΑΡΧΕΙΟ</li></a>
+        <a href="contact-us" class="m-link"><li class="m-item">CONTACT US // ΕΠΙΚΟΙΝΩΝΗΣΤΕ ΜΑΖΙ ΜΑΣ</li></a>
     </ul>
 </nav>
