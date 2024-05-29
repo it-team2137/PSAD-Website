5020896f162c1944619106843ce03b12707aad26
Bulwarini <bulwarini11@gmail.com>
2024-05-29 09:51:49 +0300

    lets go

--- courses-work.php
+++ courses-work.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="styles/nav.css">
+    <link rel="stylesheet" id="pagestyle" href="styles/style.css">
 </head>
 <body>
 <?php include_once 'navbar.php' ?>
--- index.php
+++ index.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="styles/nav.css">
+    <link rel="stylesheet" id="pagestyle" href="styles/style.css">
 </head>
 <body>
 <?php include_once 'navbar.php' ?>
@@ -23,14 +22,7 @@
     <div class="galeria"></div>
     <div class="galeria"></div>
 </section>
-<script src="./dist/dom-i18n.js"></script>
-<script>
-  var i18n = window.domI18n({
-  selector: '[data-translatable]',
-  separator: ' // ',
-  languages: ['en', 'gr'],
-  defaultLanguage: 'en',
-});
+<?php include_once 'footer.php' ?>
 </script>
 </body>
 </html>
