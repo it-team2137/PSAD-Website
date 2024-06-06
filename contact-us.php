215391458b0b0e36b3ff041053381684808143e8
cepekcepek <kubace9@gmail.com>
2024-06-06 23:40:23 +0300

    contact-us icons and text

--- contact-us.php
+++ contact-us.php
@@ -5,11 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link rel="stylesheet" id="pagestyle" href="styles/style.css">
-    <style>
-        main{
-            display: flex;
-        }
-    </style>
 </head>
 <body onload="langChange(); mode()">
 <?php include_once 'navbar.php' ?>
@@ -28,9 +23,9 @@
             </div>
             <div id="social-contact">
                 <div class="text-up">Socials</div>
-                <div id="social">Mikalis Theodosio</div>
-                <div id="social">What's Up!</div>
-                <div id="social">Instagram</div>
+                <div id="social"><img class="icons" src="\images\gallery\gallery5.jpg" alt="img prof" height="50vh">Mikalis Theodosio</div>
+                <div id="social"><img class="icons" src="\images\icons\phone.svg" alt="img prof" height="50vh">What's Up!</div>
+                <div id="social"><img class="icons" src="\images\icons\ig.svg" alt="img prof" height="50vh">Instagram</div>
             </div>
         </div>
     </div>
