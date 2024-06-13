5ce580e6307936f17fa64dfe2e1c374d035b3449
it-team2137 <it.team2137@gmail.com>
2024-06-13 12:51:12 +0300

    cyk

---
+++ head.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" id="pagestyle" href="styles/style.css">
+<link rel="preload" href="/styles/dark.css" as="style">
+<link rel="stylesheet" id="pagestyle" href="/styles/grnav.css">
+<link rel="preload" href="/styles/grnav.css" as="style">
